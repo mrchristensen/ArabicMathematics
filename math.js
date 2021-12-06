b7375b5e68321a00ef0ce229b70fc321fd10dca6
Matt Christensen <mr.christensen@gmail.com>
2021-12-05 19:30:43 -0700

    Completing the square impl

--- math.js
+++ math.js
@@ -2,3 +2,12 @@ function hello() {
   ret = "hello";
   document.getElementById("result").textContent = ret;
 }
+
+function square() {
+  a = parseInt(document.getElementById("a").value, 10);
+  b = parseInt(document.getElementById("b").value, 10) / a;
+  c = -parseInt(document.getElementById("c").value, 10) / a;
+
+  ret = Math.sqrt(c + (b / 2) ** 2) - b / 2;
+  document.getElementById("result").textContent = ret;
+}
