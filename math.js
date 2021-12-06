0102a89a759f762321ab8605198dca1516393f20
Matt Christensen <mr.christensen@gmail.com>
2021-12-05 19:57:41 -0700

    Finished law of sines

--- math.js
+++ math.js
@@ -4,10 +4,23 @@ function hello() {
 }
 
 function square() {
-  a = parseInt(document.getElementById("a").value, 10);
-  b = parseInt(document.getElementById("b").value, 10) / a;
-  c = -parseInt(document.getElementById("c").value, 10) / a;
+  a = parseInt(document.getElementById("square-a").value, 10);
+  b = parseInt(document.getElementById("square-b").value, 10) / a;
+  c = -parseInt(document.getElementById("square-c").value, 10) / a;
 
   ret = Math.sqrt(c + (b / 2) ** 2) - b / 2;
-  document.getElementById("result").textContent = ret;
+  document.getElementById("square-result").textContent = ret;
+}
+
+function sines() {
+  alpha = parseInt(document.getElementById("sines-alpha").value, 10);
+  b = parseInt(document.getElementById("sines-b").value, 10);
+  beta = parseInt(document.getElementById("sines-beta").value, 10);
+
+  // Convert radians to degrees
+  alpha *= Math.PI / 180;
+  beta *= Math.PI / 180;
+
+  ret = (b * Math.sin(alpha)) / Math.sin(beta);
+  document.getElementById("sines-result").textContent = ret;
 }
